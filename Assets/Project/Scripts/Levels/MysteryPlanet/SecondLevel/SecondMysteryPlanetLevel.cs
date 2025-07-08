6f91a73239852ef6dc378452d118326a3977315b
B1g_B0$$ <grand0077@inbox.ru>
2025-07-08 21:54:28 +0300

    Stencil написаны шейдеры под ракетную установку, которая выезжает из под земли и сделан собственный Pod для ракетной установки

--- Assets/Project/Scripts/Levels/MysteryPlanet/SecondLevel/SecondMysteryPlanetLevel.cs
+++ Assets/Project/Scripts/Levels/MysteryPlanet/SecondLevel/SecondMysteryPlanetLevel.cs
@@ -1,7 +1,29 @@
-﻿namespace Project.Scripts.Levels.MysteryPlanet.SecondLevel
+﻿using Project.Scripts.Levels.Triggers;
+using UnityEngine;
+
+namespace Project.Scripts.Levels.MysteryPlanet.SecondLevel
 {
-    public class SecondMysteryPlanetLevel
+    public class SecondMysteryPlanetLevel : Level
     {
-        
+        [SerializeField] private EnemySpawnTrigger _enemySpawnTrigger;
+        [SerializeField] private EntranceTrigger _entranceLastLvlTrigger;
+
+        private void OnEnable()
+        {
+            IsInitiatedSpawners += SpawnResources;
+        }
+
+        private void OnDisable()
+        {
+            IsInitiatedSpawners -= SpawnResources;
+        }
+
+        private void Update()
+        {
+            if (_enemySpawnTrigger.IsEnemySpawned)
+            {
+                CreateWaveOfEnemy();
+            }
+        }
     }
 }
--- Assets/Project/Scripts/Levels/MysteryPlanet/ThirdLevel/ThirdMysteryPlanetLevel.cs
+++ Assets/Project/Scripts/Levels/MysteryPlanet/ThirdLevel/ThirdMysteryPlanetLevel.cs
@@ -1,7 +1,29 @@
-﻿namespace Project.Scripts.Levels.MysteryPlanet.ThirdLevel
+﻿using Project.Scripts.Levels.Triggers;
+using UnityEngine;
+
+namespace Project.Scripts.Levels.MysteryPlanet.ThirdLevel
 {
-    public class ThirdMysteryPlanetLevel
+    public class ThirdMysteryPlanetLevel : Level
     {
-        
+        [SerializeField] private EnemySpawnTrigger _enemySpawnTrigger;
+        [SerializeField] private EntranceTrigger _entranceLastLvlTrigger;
+
+        private void OnEnable()
+        {
+            IsInitiatedSpawners += SpawnResources;
+        }
+
+        private void OnDisable()
+        {
+            IsInitiatedSpawners -= SpawnResources;
+        }
+
+        private void Update()
+        {
+            if (_enemySpawnTrigger.IsEnemySpawned)
+            {
+                CreateWaveOfEnemy();
+            }
+        }
     }
 }
