32bb134626e6a8cbbced99355098ff68fccfb49d
B1g_B0$$ <grand0077@inbox.ru>
2025-12-10 17:58:29 +0300

    Ну добавил вроде как геймплей старт для метрик но не знаю как оно будет работать

--- Assets/Project/Scripts/Game/GameRoot/GameEntryPoint.cs
+++ Assets/Project/Scripts/Game/GameRoot/GameEntryPoint.cs
@@ -50,7 +50,7 @@ namespace Project.Scripts.Game.GameRoot
 
             Application.targetFrameRate = 60;
             Screen.sleepTimeout = SleepTimeout.NeverSleep;
-
+            
             await StartGame();
 
             YG2.onShowWindowGame += _pauseService.OnPlayGame;
@@ -65,6 +65,7 @@ namespace Project.Scripts.Game.GameRoot
             YG2.onHideWindowGame -= _pauseService.OnStopGame;
             YG2.onOpenAnyAdv -= _pauseService.OnStopGame;
             YG2.onCloseAnyAdv -= _pauseService.OnPlayGame;
+            YG2.GameplayStop();
         }
 
         private async UniTask StartGame()
@@ -84,6 +85,7 @@ namespace Project.Scripts.Game.GameRoot
             }
 #endif
             Debug.Log("Старт игры, и вход в состояние загрузки");
+            YG2.GameplayStart();
 
             _uiRoot.UIStateMachine.EnterIn<LoadingPanelState>();
 
